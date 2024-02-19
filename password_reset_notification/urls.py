e1c95f01ac165292afa306ad08d49e25004843fb
Gennadii <Gennadii_s@inbox.ru>
2024-02-20 04:52:57 +1000

    fix readme

--- password_reset_notification/urls.py
+++ password_reset_notification/urls.py
@@ -3,6 +3,6 @@ from .views import reset_password, reset_password_api
 
 
 urlpatterns = [
-    path('api/reset_password/', reset_password_api, name='reset_password_api'),
+    path('api/', reset_password_api, name='reset_password_api'),
     path('request/', reset_password, name='reset_password_view'),
 ]
